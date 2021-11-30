c4f1db018a0722df72ecebc5f462c4b98d1d2dfb
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 16:53:04 +0530

    interface added to service

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Student } from './interface/student.interface';
 
 @Injectable()
 export class StudentService {
+  constructor(@InjectModel('Student') private studentModel: Model<Student>) {}
   getStudents(): string {
     return 'Student Service';
   }
