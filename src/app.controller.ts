7c9b734170f1f052e113536eaed7fe16ca1b209d
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 14:44:21 +0530

    Hello world ctrl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,12 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-// http://localhost:3000/student
-@Controller('student')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Post()
-  create(@Body('name') name: string, @Body('age') age: number): string {
-    return 'Hai ' + name + 'my age is ' + age;
-  }
-
-  @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return id;
-  }
-
-  @Put()
-  putHello(): string {
-    return 'Update the student';
   }
 }
