1d22792d9f31f330edd3f70bb4b507826efee5fd
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 15:24:55 +0530

    Student Service return

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { StudentService } from './student.service';
 
 @Controller('student')
 export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
   @Get()
   getStudent(): string {
-    return 'Student Controller';
+    return this.studentService.getStudents();
   }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  getStudents(): string {
+    return 'Student Service';
+  }
+}
