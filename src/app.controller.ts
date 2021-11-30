4593858c8bb2c704b74d56c6f1f9049c8467ea40
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 13:08:59 +0530

    Put method added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// http://localhost:3000/student
+@Controller('student')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Put()
+  putHello(): string {
+    return 'Updated student';
   }
 }
