68eda0b6788dd0ebb43e8a28c9bb290fce762a90
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 16:04:13 +0530

    Db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [StudentModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest_rest_tutorial'),
+    StudentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
