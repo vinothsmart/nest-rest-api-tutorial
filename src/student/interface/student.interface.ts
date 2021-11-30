732b7796df4758d84e25a26f5cc3d78715907a3e
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 16:51:55 +0530

    student interface

---
+++ src/student/interface/student.interface.ts
@@ -0,0 +1,7 @@
+import { Document } from 'mongoose';
+export interface Student extends Document {
+  readonly name: string;
+  readonly age: number;
+  readonly city: string;
+  readonly intial: string;
+}
