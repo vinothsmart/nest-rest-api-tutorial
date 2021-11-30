929a4c9b0832d9d41fd46465610a14874ca69dcd
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 17:24:50 +0530

    add student in mongodb

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { StudentDTO } from './dto/student.dto';
+import { Student } from './interface/student.interface';
 import { StudentService } from './student.service';
 
 @Controller('student')
@@ -12,7 +13,7 @@ export class StudentController {
   }
 
   @Post()
-  createStudent(@Body() data: StudentDTO): string {
-    return 'Hi ' + data.name;
+  async createStudent(@Body() data: StudentDTO): Promise<Student> {
+    return await this.studentService.createStudent(data);
   }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { StudentDTO } from './dto/student.dto';
 import { Student } from './interface/student.interface';
 
 @Injectable()
 export class StudentService {
   constructor(@InjectModel('Student') private studentModel: Model<Student>) {}
+
   getStudents(): string {
     return 'Student Service';
+  }
+
+  async createStudent(student: StudentDTO): Promise<Student> {
+    const newStudent = new this.studentModel(student);
+    return await newStudent.save();
   }
 }
