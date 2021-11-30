afa35a72be3cedd6d4be84c3abcc5740e4de00ac
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-30 14:41:47 +0530

    add student post api added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 // http://localhost:3000/student
@@ -11,8 +11,18 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Post()
+  create(@Body('name') name: string, @Body('age') age: number): string {
+    return 'Hai ' + name + 'my age is ' + age;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): string {
+    return id;
+  }
+
   @Put()
   putHello(): string {
-    return 'Updated student';
+    return 'Update the student';
   }
 }
